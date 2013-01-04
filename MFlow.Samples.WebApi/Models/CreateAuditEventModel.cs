2da1e237bc4d7f4ebab87f83bccd7dd661417761
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-04 19:41:40 +0000

    Sample WebApi project, showing simple validation

--- MFlow.Samples.WebApi/Models/CreateAuditEventModel.cs
+++ MFlow.Samples.WebApi/Models/CreateAuditEventModel.cs
@@ -14,9 +14,13 @@ namespace MFlow.Samples.WebApi.Models
 
             Validator
                 .Check(m => m.Description).IsNotEmpty().Message("Description cannot be empty")
-                .Check(m => m.Code).IsNotEmpty().Message("Code cannot be empty");
+                .Check(m => m.Code).IsNotEmpty().Message("Code cannot be empty")
+                .Check(m => m.SecurityKey)
+                    .Mathes(@"^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$")
+                    .Message("Security Key is invalid");
         }
 
+        public string SecurityKey { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
     }
