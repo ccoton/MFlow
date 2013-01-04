92222c98cc3a61ba20fde5960a27ed3b28f817f2
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-04 12:04:34 +0000

    #31 add a test project for MFlow.Mvc

---
+++ MFlow.Mvc.Tests/Supporting/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFlow.Mvc.Tests.Supporting
+{
+    public class LoginViewModel : ValidatedModel<LoginViewModel>
+    {
+        public LoginViewModel()
+        {
+            SetTarget(this);
+
+            Validator
+                .Check(u => u.Username).IsNotEmpty().Message("Username should not be empty, please enter a username")
+                .Check(u => u.Password).IsNotEmpty().Message("Password should not be empty, please enter a password");
+            
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MFlow.Mvc.Tests/ValidatedModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFlow.Mvc.Tests.Supporting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MFlow.Mvc.Tests
+{
+    [TestClass]
+    public class ValidatedModel
+    {
+        [TestMethod]
+        public void Test_Validated_Model_With_Errors()
+        {
+            var loginViewModel = new LoginViewModel() { Password = "", Username = "" };
+            var results = loginViewModel.Validate(new System.ComponentModel.DataAnnotations.ValidationContext(loginViewModel));
+            Assert.AreEqual(results.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Test_Validated_Model_With_No_Errors()
+        {
+            var loginViewModel = new LoginViewModel() { Password = "password", Username = "username" };
+            var results = loginViewModel.Validate(new System.ComponentModel.DataAnnotations.ValidationContext(loginViewModel));
+            Assert.AreEqual(results.Count(), 0);
+        }
+    }
+}
