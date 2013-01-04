0c5e98f3245710c424ca3f78f98e98442fd30300
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-04 19:35:58 +0000

    Sample WebApi project, showing simple validation

---
+++ MFlow.Samples.WebApi/Controllers/AuditController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using MFlow.Samples.WebApi.Models;
+
+namespace MFlow.Samples.WebApi.Controllers
+{
+    public class AuditController : ApiController
+    {
+        public string[] Get([FromUri]CreateAuditEventModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return ModelState.Values.SelectMany(m => m.Errors)
+                                 .Select(e => e.ErrorMessage)
+                                 .ToArray();
+            }
+
+            return new string[] { "Success" };
+        }
+    }
+}
---
+++ MFlow.Samples.WebApi/Models/CreateAuditEventModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MFlow.Mvc;
+
+namespace MFlow.Samples.WebApi.Models
+{
+    public class CreateAuditEventModel : ValidatedModel<CreateAuditEventModel>
+    {
+        public CreateAuditEventModel()
+        {
+            SetTarget(this);
+
+            Validator
+                .Check(m => m.Description).IsNotEmpty().Message("Description cannot be empty")
+                .Check(m => m.Code).IsNotEmpty().Message("Code cannot be empty");
+        }
+
+        public string Description { get; set; }
+        public string Code { get; set; }
+    }
+}
