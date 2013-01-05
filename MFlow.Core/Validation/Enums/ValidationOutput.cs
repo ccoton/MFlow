d756586436ff355dc9875e425939bf65c32ece9c
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-05 14:45:36 +0000

    #37 Add a validation output enum with values for Error & Warning

---
+++ MFlow.Core/Validation/Enums/ValidationOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFlow.Core.Validation.Enums
+{
+    public enum ValidationOutput
+    {
+        Error = 0,
+        Warning = 1
+    }
+}
