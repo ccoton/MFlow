0b3ee93f1d8e90174eea00cc9ee1e8eb76165587
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-12 20:17:37 +0000

    Install.ps1 script to make sure nuget package installs correctly, and copies requires resources to the output directory

--- Publish.Nuget.Build.cmd
+++ Publish.Nuget.Build.cmd
@@ -1,4 +1,5 @@
 @echo off
+rd /s /q .build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\release.build.proj %*
 powershell.exe -ExecutionPolicy ByPass -File buildscripts\publish-nuget-packages.ps1
 pause
--- Release.Build.cmd
+++ Release.Build.cmd
@@ -1,4 +1,4 @@
 @echo off
-
+rd /s /q .build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\release.build.proj %*
 pause
