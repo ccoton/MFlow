0a6edd9a2f8879a8bf706ee8782e517456962d0b
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-13 18:33:42 +0000

    Only update build number for release builds

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,4 @@
 @echo off
-powershell.exe -ExecutionPolicy ByPass -File buildscripts\set-build-version.ps1
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\build.proj %*
 rd /s /q .build
 pause
