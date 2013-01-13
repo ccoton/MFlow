d6c9bdf395a8fb88bfe5b8541a62f3905e92b870
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-13 11:59:33 +0000

    #51 Fix PostCode validator, change regex to one that satisfied running tests against both .net and mono. All tests now pass against .net and mono

--- MFlow.Core/Internal/Validators/Strings/PostCodeValidator.cs
+++ MFlow.Core/Internal/Validators/Strings/PostCodeValidator.cs
@@ -5,16 +5,16 @@ namespace MFlow.Core.Internal.Validators.Strings
 {
     class PostCodeValidator : IValidator<string>
     {
-        static IDictionary<string, bool> cache = new Dictionary<string, bool> ();
-        const string regEx = @"(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)|(([A-Z-[QVX]][0-9][A-HJKSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY])))) [0-9][A-Z-[CIKMOV]]{2})";
-        static Regex reg = new Regex (regEx, RegexOptions.IgnoreCase);
+        static IDictionary<string, bool> cache = new Dictionary<string, bool>();
+        const string regEx = @"^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$";
+        static Regex reg = new Regex(regEx, RegexOptions.IgnoreCase);
 
-        public bool Validate (string input)
+        public bool Validate(string input)
         {
-            if (string.IsNullOrEmpty (input))
+            if (string.IsNullOrEmpty(input))
                 return false;
-            if (!cache.ContainsKey (input))
-                cache [input] = reg.IsMatch (input);
+            if (!cache.ContainsKey(input))
+                cache [input] = reg.IsMatch(input);
             return cache [input];
         }
     }
