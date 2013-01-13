db090df93d11e1fe108f6d1bfd7a94f50236eef6
Mark Woodhall <mark.woodhall@gmail.com>
2013-01-13 16:00:09 +0000

    Self incrementing revision and minor build numbers in appropriate build scripts. Created new tag

--- Publish.Nuget.Build.cmd
+++ Publish.Nuget.Build.cmd
@@ -1,5 +1,7 @@
 @echo off
-rd /s /q .build
+powershell.exe -ExecutionPolicy ByPass -File buildscripts\set-release-version.ps1
+path=%path%;C:\Program Files (x86)\Git\cmd
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\release.build.proj %*
 powershell.exe -ExecutionPolicy ByPass -File buildscripts\publish-nuget-packages.ps1
+rd /s /q .build
 pause
---
+++ buildscripts/set-release-version.ps1
@@ -0,0 +1,47 @@
+function Update-SourceVersion ( $updatedVersion )
+{ 
+	echo $updatedVersion 
+	$NewVersion = 'AssemblyVersion("' + $updatedVersion + '")';
+	$NewFileVersion = 'AssemblyFileVersion("' + $updatedVersion + '")';
+
+	echo $NewVersion
+	foreach ($o in $input) 
+	{
+	Write-output $o.FullName
+	$TmpFile = $o.FullName + ".tmp"
+
+	 get-content $o.FullName | 
+		%{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewVersion } |
+		%{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewFileVersion }  > $TmpFile
+
+	 move-item $TmpFile $o.FullName -force
+	}
+}
+
+
+function Update-AllAssemblyInfoFiles ( $updatedVersion )
+{
+	foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) 
+	{
+		echo $Version
+		get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceVersion $updatedVersion ;
+	}
+}
+
+$scriptpath = split-path -parent $MyInvocation.MyCommand.Path
+$buildnumberfile = resolve-path "$scriptpath/../buildnumber.xml"
+#read in the xml
+$xml = [xml](Get-Content $buildnumberfile)
+
+#get the name of the app pool
+$major = $xml.BuildNumber.Major
+$minor =  [string]([int]$xml.BuildNumber.Minor + 1)
+$version = $xml.BuildNumber.Version
+$revision = $xml.BuildNumber.Revision
+$updatedVersion = $major + "." + $minor + "." + $version + "." + $revision
+Update-AllAssemblyInfoFiles $updatedVersion;
+$xml.BuildNumber.Major = $major
+$xml.BuildNumber.Minor = $minor
+$xml.BuildNumber.Version = $version
+$xml.BuildNumber.Revision = $revision
+$xml.Save($buildnumberfile)
