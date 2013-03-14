e83441048c9e17fe4b1555f952635361e897458d
markwoodhall <mark.woodhall@gmail.com>
2013-03-14 19:49:28 +0000

    Migrate last test to mspec

--- MFlow.Core.Tests/for_FluentValidation/when_calling_validate.cs
+++ MFlow.Core.Tests/for_FluentValidation/when_calling_validate.cs
@@ -15,10 +15,13 @@ namespace MFlow.Core.Tests.for_FluentValidation
 
         Because of = () => { results = validator.Validate().ToList(); };
 
-        It should_contain_the_correct_number_of_validation_results = () => { results.Count.ShouldEqual(2); };
+        It should_contain_the_correct_number_of_validation_results = () => { results.Count.ShouldEqual(3); };
         It should_contain_a_first_result_with_the_correct_key = () =>{results.First().Condition.Key.ShouldEqual("Password");};
         It should_contain_a_second_result_with_the_correct_key = () => { results.Skip(1).First().Condition.Key.ShouldEqual("ConfirmPassword"); };
+        It should_contain_a_third_result_with_the_correct_key = () => { results.Skip(2).First().Condition.Key.ShouldEqual("Manager.Username"); };
         It should_contain_a_first_result_with_the_correct_message = () => { results.First().Condition.Message.ShouldEqual("Password should be equal to testingx"); };
         It should_contain_a_second_result_with_the_correct_message = () => { results.Skip(1).First().Condition.Message.ShouldEqual("ConfirmPassword should be equal to testing"); };
+        It should_contain_a_third_result_with_the_correct_message = () => { results.Skip(2).First().Condition.Message.ShouldEqual("Manager.Username should not be empty"); };
+ 
     }
 }
