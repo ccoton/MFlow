ef62fdc7cea5a33d53f10d84d8729291ab2a2e64
markwoodhall <mark.woodhall@gmail.com>
2013-03-21 20:03:37 +0000

    #97 custom This Week validator and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/custom_validators/ThisWeekValidator.cs
@@ -0,0 +1,17 @@
+﻿using MFlow.Core.Internal.Validators.Dates;
+using MFlow.Core.Internal.Validators.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation.custom_validators
+{
+    public class ThisWeekValidator : IThisWeekValidator
+    {
+        public bool Validate(DateTime input)
+        {
+            return input == DateTime.Parse("01-01-2001");
+        }
+    }
+}
