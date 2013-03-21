f5096d63c166d098842505af7c9d51cfc2ddde86
markwoodhall <mark.woodhall@gmail.com>
2013-03-21 20:13:45 +0000

    #95 add custom IsThisYear and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/when_calling_is_this_year_with_a_value_that_satisfies_a_custom_this_year_validator.cs
@@ -0,0 +1,21 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class when_calling_is_this_year_with_a_value_that_satisfies_a_custom_this_year_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
+    {
+
+        Because of = () =>
+        {
+            user.LastLogin = DateTime.Parse("01-01-2001");
+            validator.Check(u => u.LastLogin).IsThisYear();
+        };
+
+        It should_be_satisfied = () => { validator.Satisfied().ShouldBeTrue(); };
+
+    }
+}
