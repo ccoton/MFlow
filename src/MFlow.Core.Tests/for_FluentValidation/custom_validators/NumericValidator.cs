b5e11e0a170f83762fd16a245961d3541c71d811
markwoodhall <mark.woodhall@gmail.com>
2013-03-21 21:07:18 +0000

    #83 custom IsNumeric and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/custom_validators/NumericValidator.cs
@@ -0,0 +1,16 @@
+﻿using MFlow.Core.Internal.Validators.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation.custom_validators
+{
+    public class NumericValidator : INumericValidator
+    {
+        public bool Validate(string input)
+        {
+            return input == "customnumericvalidator";
+        }
+    }
+}
---
+++ src/MFlow.Core.Tests/for_FluentValidation/when_calling_is_numeric_with_a_value_that_satisfies_a_custom_numeric_validator.cs
@@ -0,0 +1,21 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class when_calling_is_numeric_with_a_value_that_satisfies_a_custom_numeric_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
+    {
+
+        Because of = () =>
+        {
+            user.Password = "customnumericvalidator";
+            validator.Check(u => u.Password).IsNumeric();
+        };
+
+        It should_be_satisfied = () => { validator.Satisfied().ShouldBeTrue(); };
+
+    }
+}
