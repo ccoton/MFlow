358e3afeeca788c4b4fa1c4b6e78d67731e81861
markwoodhall <mark.woodhall@gmail.com>
2013-03-22 09:05:02 +0000

    #89 custom IsGreaterThan and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/custom_validators/GreaterThanValidator.cs
@@ -0,0 +1,16 @@
+﻿using MFlow.Core.Internal.Validators.Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation.custom_validators
+{
+    public class GreaterThanValidator : ILessThanValidator
+    {
+        public bool Validate(int input, int value)
+        {
+            return input == 100;
+        }
+    }
+}
