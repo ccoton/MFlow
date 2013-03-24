84bb192931e72a19216710aaf6d27bd3064eadac
markwoodhall <mark.woodhall@gmail.com>
2013-03-24 21:41:28 +0000

    #fix up #92 and add #93 custom IsAfter and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/after/when_calling_is_after_than_with_a_value_that_does_not_satisfy_a_custom_after_validator.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class when_calling_is_after_with_a_value_that_does_not_satisfy_a_custom_after_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
+    {
+
+        Because of = () =>
+        {
+            user.LastLogin = DateTime.Parse("01-01-2001");
+            validator.Check(u => u.LastLogin).IsAfter(user.LastLogin.Value);
+        };
+
+        It should_not_be_satisfied = () => { validator.Satisfied().ShouldBeFalse(); };
+        It should_return_the_correct_validation_message = () => { validator.Validate().First().Condition.Message.ShouldEqual("LastLogin should be after 01/01/2001 00:00:00"); };
+
+    }
+}
--- src/MFlow.Core.Tests/for_FluentValidation/after/when_calling_is_after_with_a_value_that_satisfies_a_custom_after_validator.cs
+++ src/MFlow.Core.Tests/for_FluentValidation/after/when_calling_is_after_with_a_value_that_satisfies_a_custom_after_validator.cs
@@ -6,13 +6,13 @@ using System.Text;
 
 namespace MFlow.Core.Tests.for_FluentValidation
 {
-    public class when_calling_is_before_than_with_a_value_that_satisfies_a_custom_before_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
+    public class when_calling_is_after_with_a_value_that_satisfies_a_custom_after_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
     {
 
         Because of = () =>
         {
             user.LastLogin = DateTime.Parse("01-01-2000");
-            validator.Check(u => u.LastLogin).IsBefore(user.LastLogin.Value);
+            validator.Check(u => u.LastLogin).IsAfter(user.LastLogin.Value);
         };
 
         It should_be_satisfied = () => { validator.Satisfied().ShouldBeTrue(); };
