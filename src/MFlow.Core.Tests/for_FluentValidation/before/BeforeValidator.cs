f2a6153742fa1e94d3d92eccc8426b0ef8e57253
markwoodhall <mark.woodhall@gmail.com>
2013-03-24 21:37:26 +0000

    #92 custom IsBefore and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/before/BeforeValidator.cs
@@ -0,0 +1,17 @@
+﻿using MFlow.Core.Internal.Validators.Dates;
+using MFlow.Core.Internal.Validators.Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class BeforeValidator : IBeforeValidator
+    {
+        public bool Validate(DateTime input, DateTime value)
+        {
+            return input.Year == 2000;
+        }
+    }
+}
