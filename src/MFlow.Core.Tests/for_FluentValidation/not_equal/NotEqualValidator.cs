2f29b134d11c1ecf51b714350d032aa262a8d62f
markwoodhall <mark.woodhall@gmail.com>
2013-03-25 21:26:55 +0000

    #100 custom IsNotEqual and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/not_equal/NotEqualValidator.cs
@@ -0,0 +1,17 @@
+﻿using MFlow.Core.Internal.Validators.Generic;
+using MFlow.Core.Internal.Validators.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class NotEqualValidator<TInput, TCompare> : INotEqualToValidator<TInput, TCompare>
+    {
+        public bool Validate(TInput input, TCompare value)
+        {
+            return input.ToString() == "customnotequalvalidator";
+        }
+    }
+}
