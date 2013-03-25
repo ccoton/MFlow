dd90a04ed9554e851a32889e84e67de25ea4af78
markwoodhall <mark.woodhall@gmail.com>
2013-03-25 21:23:40 +0000

    #99 custom IsEqual and spec

---
+++ src/MFlow.Core.Tests/for_FluentValidation/equal_to/when_calling_is_equal_to_with_a_value_that_does_not_satisfy_a_custom_equal_validator.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFlow.Core.Tests.for_FluentValidation
+{
+    public class when_calling_is_equal_with_a_value_that_does_not_satisfy_a_custom_equal_validator : given.a_fluent_user_validator_with_custom_implementation_set_to_replace
+    {
+
+        Because of = () =>
+        {
+            user.Password = "doesnotmatchcustomrequiredvalidator";
+            validator.Check(u => u.Password).IsEqualTo("");
+        };
+
+        It should_not_be_satisfied = () => { validator.Satisfied().ShouldBeFalse(); };
+        It should_return_the_correct_validation_message = () => { validator.Validate().First().Condition.Message.ShouldEqual("Password should be equal to "); };
+
+    }
+}
